d59c63fcd2c00a504ab422f23a0ebea7b4af048d
Niyobuhungiro Yves <niyobuhungiro.yves@gmail.com>
2022-05-18 18:16:11 +0200

    adding test random_attendance

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -22,14 +22,31 @@ class HelpersTest extends TestCase
     {
 
         $over18_years = now()->subYears(19)->format("Y-m-d");
+
         $this->assertTrue(isOver18($over18_years));
         $this->assertFalse(!isOver18($over18_years));
         $this->assertIsBool(isOver18($over18_years));
 
 
         $under18_years = now()->subYears(17)->format("Y-m-d");
+
         $this->assertFalse(isOver18($under18_years));
         $this->assertTrue(!isOver18($under18_years));
         $this->assertIsBool(isOver18($under18_years));
+    }
+
+    /**
+     * @test
+     */
+    public function test_can_generate_random_attendance_time()
+    {
+
+        $times = genereateFakeAttendance();
+
+        $this->assertIsArray($times);
+        $this->assertArrayHasKey("arrived_at", $times);
+        $this->assertArrayHasKey("left_at", $times);
+        $this->assertNotNull($times["arrived_at"]);
+        $this->assertNotNull($times["left_at"]);
     }
 }
